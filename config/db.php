83ac29bd0ee87547ac2081ad9c0e4a99fffed37b
Dolgov Dmitriy <TwilightTower@mail.ru>
2017-06-05 01:55:14 +0300

    first commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=news_news',
+    'username' => 'root',
+    'password' => 'temp123',
+    'charset' => 'utf8',
+];
