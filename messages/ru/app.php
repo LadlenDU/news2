871fcf47fe41e7edd39a0ce583a2c2b317e0c319
Dolgov Dmitriy <TwilightTower@mail.ru>
2017-06-11 09:11:33 +0300

    news translations

---
+++ messages/ru/app.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'News' => 'Новости',
+    'Title' => 'Заголовок',
+    'Body' => 'Содержание',
+    'Created At' => 'Создано',
+    'Updated At' => 'Обновлено',
+    'Create Article' => 'Создать статью',
+    'Update' => 'Обновить',
+    'Delete' => 'Удалить',
+    'Update Article: ' => 'Обновить статью: ',
+];
+
