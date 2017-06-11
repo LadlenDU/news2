74ae58b6f5a84045ebcb6f789c0bc4fe1b9989fe
Dolgov Dmitriy <TwilightTower@mail.ru>
2017-06-11 09:01:00 +0300

    news to admin moved

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -6,8 +6,8 @@ use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
-use app\models\LoginForm;
-use app\models\ContactForm;
+//use app\models\LoginForm;
+//use app\models\ContactForm;
 
 class SiteController extends Controller
 {
