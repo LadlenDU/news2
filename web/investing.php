620ec480fd6e8e859e5fcf36551cb1442c9ed368
Dolgov Dmitriy <TwilightTower@mail.ru>
2017-06-12 21:03:43 +0300

    -- investing

--- web/investing.php
+++ web/investing.php
@@ -5,6 +5,7 @@ ini_set('display_errors', 1);
 
 define('ERR_LOG_FILE', dirname(__FILE__) . '/investing_scraper_error.txt');
 define('URL', 'https://ru.investing.com/technical/%D0%A1%D0%B2%D0%BE%D0%B4%D0%BD%D1%8B%D0%B9-%D0%A2%D0%B5%D1%85%D0%BD%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9-%D0%90%D0%BD%D0%B0%D0%BB%D0%B8%D0%B7');
+define('TABLE_FILE', dirname(__FILE__) . '/gen_table.html');
 
 $ch = curl_init();
 
@@ -59,15 +60,21 @@ for ($r = 0; $r < $rows->length; ++$r) {
         if ($pos == 0) {
             $td = $tr->item(0);
             if (strpos($td->textContent, 'RUB') === false) {
-                $column['head'] = $aElem->textContent;
+                $column['head'] = $td->textContent;
             } else {
                 $r += 2;
                 continue;
             }
         } elseif ($pos == 2) {
-            for ($c = 2; $c < 4; ++$c) {
+            for ($c = 2; $c <= 4; ++$c) {
                 if ($td = $tr->item($c)) {
-                    //$column['data'][] =
+                    if (strpos($td->textContent, 'Активно продавать') !== false
+                        || strpos($td->textContent, 'Активно покупать') !== false
+                    ) {
+                        $column['data'][] = $td->textContent;
+                    } else {
+                        $column['data'][] = '';
+                    }
                 } else {
                     errLog('Не удалось получить ячейку элемента ' . print_r($tr, true));
                 }
@@ -85,7 +92,38 @@ for ($r = 0; $r < $rows->length; ++$r) {
 
 print_r($table);
 
+$html = createTable($table);
+file_put_contents(TABLE_FILE, $html);
+
 function errLog($text)
 {
     file_put_contents(ERR_LOG_FILE, date(DATE_ATOM) . ": $text\n", FILE_APPEND);
+}
+
+function createTable($info)
+{
+
+    $t = "<table>\n";
+
+    // Header
+    $t .= "<thead>\n<tr>\n";
+    foreach ($info as $el) {
+        $t .= '<td>' . $el['head'] . "</td>\n";
+
+    }
+    $t .= "</tr>\n</thead>\n";
+
+    // Body
+    $t .= "<tbody>\n";
+    for ($i = 0; $i < 3; ++$i) {
+        foreach ($info as $el) {
+            $t .= '<td>' . ($el['data'][$i] ? $el['data'][$i] : '&nbsp;') . "</td>\n";
+
+        }
+    }
+    $t .= "</tbody>\n";
+
+    $t .= "</table>";
+
+    return $t;
 }
